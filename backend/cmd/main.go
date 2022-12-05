6534916ffcc57d05a8424fec6547394f6ab3b39d
0xpeIpeI <hokumaru12@gmail.com>
2022-12-05 22:29:51 +0900

    ユーザ作成メソッド追加

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	container "github.com/lll-lll-lll-lll/youtube-url-converter-backend/lib/container"
+	fb "github.com/lll-lll-lll-lll/youtube-url-converter-backend/lib/firebase"
+	"github.com/rs/xid"
 
 	db "github.com/lll-lll-lll-lll/youtube-url-converter-backend/lib/db"
 )
@@ -15,6 +19,10 @@ func main() {
 	postgresql := db.NewPostgreSql()
 	postgresql.Open()
 	defer postgresql.Db.Close()
+	firebaseApp, err := fb.InitFireBase()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	router.POST("/invitation_code", func(ctx *gin.Context) {
 		var input container.Input
 		if err := ctx.BindJSON(&input); err != nil {
@@ -31,9 +39,27 @@ func main() {
 			return
 		}
 		ctx.JSON(http.StatusCreated, gin.H{"message": con.Code})
-
 	})
+	router.POST("/register", RegisterHandler(firebaseApp))
 	router.Run(":8080")
+}
+
+func RegisterHandler(firebaseApp *fb.FirebaseApp) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var input fb.RegisterUserBody
+		if err := ctx.BindJSON(&input); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "faild to bind json"})
+			return
+		}
+		userID := xid.New().String()
+		req := &fb.RegisterUser{ID: userID, Email: input.Email, Password: input.Password}
+		record, err := fb.CreateUserWithUID(ctx, firebaseApp.Client, req)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("ユーザ作成に失敗しました")})
+			return
+		}
+		ctx.JSON(http.StatusCreated, gin.H{"message": record.UID})
+	}
 }
 
 func transaction(db *sqlx.DB, req interface{}, f func(req interface{}, db *sqlx.DB) error) error {
