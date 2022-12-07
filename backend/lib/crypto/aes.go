b9ecb68ef8da04d3fd57547ceec2d11c032d9eda
0xpeIpeI <hokumaru12@gmail.com>
2022-12-07 17:10:03 +0900

    modify: 返り値変更

--- backend/lib/crypto/aes.go
+++ backend/lib/crypto/aes.go
@@ -18,7 +18,7 @@ func NewAES(key string) (cipher.Block, error) {
 func GenerateIV() ([]byte, error) {
 	iv := make([]byte, aes.BlockSize)
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return []byte(""), err
+		return nil, err
 	}
 	return iv, nil
 }
